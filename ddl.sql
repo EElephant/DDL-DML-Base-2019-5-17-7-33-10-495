c46cda8da9feedb4c7852752723b2d6754dcdd74
Ethan <782227508@qq.com>
2019-07-03 20:36:25 +0800

    ddl dml task

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,32 @@
 -- **数据库级别：**  
 --  显示所有数据库  
-
+show database;
 --  进入某个数据库  
-
+use myDatebase;
 --  创建一个数据库  
-
+create database if not exist myDatebase;
 --  创建指定字符集的数据库  
-
+create database if not exist myDatebase default charset utf8;
 --  显示数据库的创建信息   
-
+show create database myDatebase;
 --  修改数据库的编码  
-
+alter database myDatebase character set gbk;
 --  删除一个数据库   
-
+drop database myDatebase;
 -- **表级别**
 --  修改表名
-
+alter table myDatebase rename to myDatebase1;
 --  修改字段的数据类型
-
+alter table myDatebase modify xxx varchar(10);
 --  修改字段名
-
+alter table myDatebase change xxx yyy varchar(10);
 --  添加字段
-
+alter table myDatebase add zzz varchar(20);
 --  删除字段
-
+alter table myDatebase drop zzz;
 --  修改表的存储引擎
-
+alter table myDatebase engine=MYISAM;
 --  删除表的外键约束
-
+alter table myDatebase drop foreign key xxx;
 --  删除一张表
+drop table myDatebase;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- 插入记录  
-
+INSERT INTO `student` VALUES ('001', 'Tom', 'man');
 -- 修改记录  
-
---  删除记录  
-
--- 查询记录  
+UPDATE student set id='002' where Sname='Tom';
+-- 删除记录
+DELETE FROM student WHERE id ='003';
+-- 查询记录
+SELECT * from student where id='003';
